089e5e2b2174c176869ee767c0e30e35107b164f
root <root@localhost.localdomain>
2017-09-05 10:32:43 +0200

    pendu

---
+++ pendu.py
@@ -0,0 +1,34 @@
+##########################################
+#title - jeu le pendu                    #
+#date - 04/09/2017                       #
+##########################################
+
+################# importation fonction externe #######
+
+from random import *
+
+################# fonction ###########################
+def ListeDeMots ():
+	mot = ["maison", "voiture", "esd"]
+	atrouver = random.choise(mot)
+	return null
+
+def MotTrouver ():
+	pendu = 1
+	 
+	for i, elt in enumerate(mot):
+		while pendu <= 10:
+			if m != ListeDeMots
+
+			print faux
+			
+			pendu=pendu+1
+	else 
+		print correct
+
+################# body ################################
+
+print ("bienvenue au jeu le pendu")
+print ListeDeMots
+m = input ("donner une lettre du mot a touver : ")
+MotTrouver = m
